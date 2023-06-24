8b8619bf8e7923594ed491b7732a7433f3424c20
salama-alkaabi <sal611592@gmail.com>
2023-06-24 14:53:20 +0400

    Initial Commit

---
+++ Cookies/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  Cookies
+//
+//  Created by sal on 24/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var counter = 0
+    @State var message = ""
+    var body: some View {
+        VStack {
+            
+            Text("\(counter) cookies eaten")
+                .font(.title)
+                .bold()
+            Button{
+                if(counter >= 40){
+                    message = "OH NOOOO!!! 😱"
+                }
+                else if(counter > 30){
+                    message = " Wha are you doing?? 😰"
+                }
+                else if(counter >= 20){
+                    message = " Don't eat too many cookies!! ☹️"
+                }
+                self.counter += 1
+                print("Bye Bye cookie")
+                
+            }label:{
+                Text("OM NOM NOM")
+            }
+            .padding()
+            .buttonStyle(.borderedProminent)
+            .cornerRadius(10)
+            .shadow(color: .white, radius: 5, x: 0.0, y: 0.0)
+            
+                        Text(message)
+                .padding()
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cookies/CookiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookiesApp.swift
+//  Cookies
+//
+//  Created by sal on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CookiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
